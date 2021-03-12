c41ead9f06b1fa74f3bdbd33e9024fddc1964a08
DiyaNagesia <73646449+DiyaNagesia@users.noreply.github.com>
2021-03-12 11:37:36 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,59 +1,49 @@
-
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-var engine, world;
 
+var engine, world;
 var holder,ball,ground;
 var stand1,stand2;
 var ball;
 var slingShot;
 var polygon_img;
-
 function preload(){
- polygon_img=loadImage("polygon.png");
-
+  polygon_img=loadImage("polygon.png");
 }
-
 function setup() {
-  engine  = Engine.create();
-  world = engine.world;
-
   createCanvas(900,400);
-  ground = new Ground(500,300,50,10);
-  stand1 = new Ground(380,300,270,10);
-  stand2 = new Ground(700,200,200,10);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground();
+  stand1 = new Stand(390,300,250,10);
+  stand2 = new Stand(700,200,200,10);
  
   //level one
-  block1 = new Block(280,275,30,40);  
-  block2 = new Block(310,275,30,40);
-  block3 = new Block(340,275,30,40);
-  block4 = new Block(370,275,30,40);
-  block5 = new Block(400,275,30,40);
-  block6 = new Block(430,275,30,40);
-  block7 = new Block(460,275,30,40);
-  block8 = new Block(490,275,30,40);
+  block1 = new Block(300,275,30,40);
+  console.log(block1);
+  block2 = new Block(330,275,30,40);
+  block3 = new Block(360,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(420,275,30,40);
+  block6 = new Block(450,275,30,40);
+  block7 = new Block(480,275,30,40);
   //level two
-  block9 = new Block(310,235,30,40);
-  block10 = new Block(340,235,30,40);
-  block11 = new Block(370,235,30,40);
-  block12 = new Block(400,235,30,40);
-  block13 = new Block(430,235,30,40);
-  block14 = new Block(460,235,30,40);
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
   //level three
-  block15 = new Block(340,195,30,40);
-  block16 = new Block(370,195,30,40);
-  block17 = new Block(400,195,30,40);
-  block18 = new Block(430,195,30,40);
-  //level four
-  block19 = new Block(370,155,30,40);
-  block20 = new Block(400,155,30,40);
-  //level five
-  block21 = new Block(385,115,30,40);
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  //top
+  block16 = new Block(390,155,30,40);
 
-  //set two 
+  //set 2 for second stand
   //level one
   blocks1 = new Block(640,175,30,40);
   blocks2 = new Block(670,175,30,40);
@@ -64,31 +54,31 @@ function setup() {
   blocks6 = new Block(670,135,30,40);
   blocks7 = new Block(700,135,30,40);
   blocks8 = new Block(730,135,30,40);
-  //level three
+  //top
   blocks9 = new Block(700,95,30,40);
 
-  //ball  with slings
+  //ball holder with slings
   ball = Bodies.circle(50,200,20);
   World.add(world,ball);
 
-  slingShot = new SlingShot(this.ball,{x:100,y:200});
+  slingShot = new Slingshot(this.ball,{x:100,y:200});
 
 }
-
 function draw() {
-
- background("#3C2E2E")
-  Engine.update(engine);
+  background(56,44,44); 
  
-  strokeWeight(2);
-  stroke(15);
-  
-  ground.display()
+  //Engine.update(engine);
+  //text(mouseX + ',' + mouseY, 10, 15);
+  textSize(20);
+  fill("lightyellow");
+  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
+
+  ground.display();
   stand1.display();
   stand2.display();
-  
+  strokeWeight(2);
   stroke(15);
-  fill("#67D1EA")
+  fill("skyblue");
   block1.display();
   block2.display();
   block3.display();
@@ -96,61 +86,39 @@ function draw() {
   block5.display();
   block6.display();
   block7.display();
+  fill("pink");
   block8.display();
-  stroke(15)
-  fill("#FFBEC4")
   block9.display();
   block10.display();
   block11.display();
   block12.display();
+  fill("turquoise");
   block13.display();
   block14.display();
- stroke(15)
- fill("#00E4D0")
   block15.display();
+  fill("grey");
   block16.display();
-  block17.display();
-  block18.display();
-    stroke(15)
-    fill("#807D78")
-  block19.display();
-  block20.display();
- stroke(15)
- fill("orange")
-  block21.display();
-
-  stroke(15)
-  fill("#67D1EA")
+  fill("skyblue");
   blocks1.display();
   blocks2.display();
   blocks3.display();
   blocks4.display();
   blocks5.display();
-  stroke(15)
-  fill("#00E4D0")
+  fill("turquoise");
   blocks6.display();
   blocks7.display();
   blocks8.display();
-  stroke(15)
-  fill("#FFBEC4")
+  fill("pink")
   blocks9.display();
   fill("gold");
-   text("Drag the Hexagon Stone and release to, launch it towards the blocks",100,30)
-  textSize(20)
-  fill("yellow")
-  //imageMode(CENTER)
-  //ball = loadImage("polygon_img,ball.position.x-50,ball.position.y-80,40,40")
-  //ellipseMode(RADIUS)
-  ellipse(ball.position.x,ball.position.y,40)
-
+  imageMode(CENTER)
+  image(polygon_img ,ball.position.x,ball.position.y,40,40);
 
   slingShot.display();
 }
-
 function mouseDragged(){
   Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
 }
-
 function mouseReleased(){
   slingShot.fly();
 }
